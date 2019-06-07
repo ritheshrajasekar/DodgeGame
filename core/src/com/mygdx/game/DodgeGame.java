e91ca3ee785575b0167b84c5aa00c9f68c28f97f
Mathew Seng <gigamatt123@gmail.com>
2019-06-07 07:54:34 -0400

    .

--- core/src/com/mygdx/game/DodgeGame.java
+++ core/src/com/mygdx/game/DodgeGame.java
@@ -10,8 +10,8 @@ import com.mygdx.game.com.mygdx.game.screens.MenuScreen;
  */
 public class DodgeGame extends Game {
 
-	public static final int WIDTH = 1200;
-	public static final int HEIGHT = 900;
+	public static final int WIDTH = 1280;
+	public static final int HEIGHT = 720;
 	public static final String TITLE = "Dodge";
 	public SpriteBatch batch;
 

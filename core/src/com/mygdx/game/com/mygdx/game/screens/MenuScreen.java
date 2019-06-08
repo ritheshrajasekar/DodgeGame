b71955ab315d91c8d8a0c831e0d1bfbaac46e6d4
rithesh.rajasekar@gmail.com <facebook77R>
2019-06-08 16:56:54 -0400

    .

--- core/src/com/mygdx/game/com/mygdx/game/screens/MenuScreen.java
+++ core/src/com/mygdx/game/com/mygdx/game/screens/MenuScreen.java
@@ -37,7 +37,7 @@ public class MenuScreen implements Screen {
 
     }
     public void render(float delta){
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.begin();
 

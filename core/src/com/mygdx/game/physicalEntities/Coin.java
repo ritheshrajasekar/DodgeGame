6714629a7272274e930af71e4b4806024e6d16ab
Mathew Seng <gigamatt123@gmail.com>
2019-06-08 17:33:22 -0400

    .

--- core/src/com/mygdx/game/physicalEntities/Coin.java
+++ core/src/com/mygdx/game/physicalEntities/Coin.java
@@ -8,19 +8,17 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import static com.mygdx.game.com.mygdx.game.screens.Level1.*;
 
-public class Coin {
+public class Coin extends Entity{
     Texture coin;
     TextureRegion animationFrames[];
     Animation animation;
     float elapsedTime;
-    int xCoord;
-    int yCoord;
 
     public Coin(){
         coin = new Texture("dodgeBigCoin.png");
         TextureRegion[][] tmpFrames = TextureRegion.split(coin,8,8);
-        xCoord = (int)(8 * Math.random());
-        yCoord = (int)(8 * Math.random());
+        setX((int)(8 * Math.random()));
+        setY((int)(8 * Math.random()));
         animationFrames = new TextureRegion[24];
         int index = 0;
 
@@ -32,24 +30,13 @@ public class Coin {
 
         animation = new Animation(1f/24f,animationFrames);
     }
-    public int getX(){
-        return xCoord;
-    }
-    public int getY() { return yCoord; }
-    public int xCoordToPixel(int x) {
-        return x * PLAYER_MOVE_DISTANCE + GRID_OFFSET_X;
-    }
-
-    public int yCoordToPixel(int y) {
-        return y * PLAYER_MOVE_DISTANCE + GRID_OFFSET_Y;
-    }
 
     public void update(){
         elapsedTime += Gdx.graphics.getDeltaTime();
     }
     public void render(SpriteBatch batch){
 
-        batch.draw(animation.getKeyFrame(elapsedTime,true), xCoordToPixel(xCoord) + GRID_CORNER_SIZE, yCoordToPixel(yCoord) + GRID_CORNER_SIZE, COIN_SIZE, COIN_SIZE);
+        batch.draw(animation.getKeyFrame(elapsedTime,true), xCoordToPixel(getX()) + GRID_CORNER_SIZE, yCoordToPixel(getY()) + GRID_CORNER_SIZE, COIN_SIZE, COIN_SIZE);
     }
 
 

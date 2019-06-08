76772427b8b717041102853e90a70f39c0ef0c3b
rithesh.rajasekar@gmail.com <facebook77R>
2019-06-08 18:29:05 -0400

    .

--- core/src/com/mygdx/game/com/mygdx/game/screens/StartScreen.java
+++ core/src/com/mygdx/game/com/mygdx/game/screens/StartScreen.java
@@ -5,29 +5,41 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.mygdx.game.DodgeGame;
 
-public class MenuScreen implements Screen {
+import java.awt.*;
+
+public class StartScreen implements Screen {
     private DodgeGame game;
     private Texture exitButtonInactive;
     private Texture exitButtonActive;
     private Texture playButtonInactive;
     private Texture playButtonActive;
+    private Texture startScreenBackground;
     private static final int EXIT_BUTTON_WIDTH = 300;
     private static final int EXIT_BUTTON_HEIGHT = 150;
     private static final int PLAY_BUTTON_WIDTH = 300;
     private static final int PLAY_BUTTON_HEIGHT = 150;
+    private static final int BACKGROUND_WIDTH = DodgeGame.WIDTH;
+    private static final int BACKGROUND_HEIGHT = DodgeGame.HEIGHT;
     private static final int EXIT_BUTTON_YVALUE = (int)(DodgeGame.HEIGHT * 0.1);
     private static final int PLAY_BUTTON_YVALUE = (int)(DodgeGame.HEIGHT * 0.4);
     private Music music;
 
+    public static Sprite backgroundSprite;
+
     //private timer Timer;
-    public MenuScreen(DodgeGame game){
+    public StartScreen(DodgeGame game){
         this.game = game;
         exitButtonInactive = new Texture("exitButton.jpg");
         exitButtonActive = new Texture("exitButtonActive.jpg");
         playButtonInactive = new Texture("playButton2.jpg");
         playButtonActive = new Texture("playButton2Active.jpg");
+        startScreenBackground = new Texture("dodgeStartScreen.png");
+        backgroundSprite = new Sprite(startScreenBackground);
+        backgroundSprite.scale(7);
+
         music = Gdx.audio.newMusic(Gdx.files.internal("myName.mp3"));
         music.setLooping(true);
         music.setVolume(1f);
@@ -36,10 +48,15 @@ public class MenuScreen implements Screen {
     public void show(){
 
     }
+
     public void render(float delta){
-        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClearColor(0.5f,0.5f,0.5f,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.begin();
+        backgroundSprite.setPosition(DodgeGame.WIDTH / 2 - backgroundSprite.getWidth()/2, DodgeGame.HEIGHT/2 - backgroundSprite.getHeight()/2);
+        backgroundSprite.draw(game.batch);
+        //game.batch.draw(startScreenBackground, DodgeGame.WIDTH/2 - BACKGROUND_WIDTH/2, DodgeGame.HEIGHT - BACKGROUND_HEIGHT/2, BACKGROUND_WIDTH, BACKGROUND_HEIGHT);
+
 
         if(Gdx.input.getX() < DodgeGame.WIDTH/2  + EXIT_BUTTON_WIDTH /2 &&
             Gdx.input.getX() > DodgeGame.WIDTH/2 - EXIT_BUTTON_WIDTH/2 &&

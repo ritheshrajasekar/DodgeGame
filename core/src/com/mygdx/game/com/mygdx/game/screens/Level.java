03fcf3a327bafa01c55623ff15cf0cd0225633fb
Mathew Seng <gigamatt123@gmail.com>
2019-06-09 02:21:28 -0400

    .

--- core/src/com/mygdx/game/com/mygdx/game/screens/Level.java
+++ core/src/com/mygdx/game/com/mygdx/game/screens/Level.java
@@ -1,13 +1,13 @@
 package com.mygdx.game.com.mygdx.game.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.mygdx.game.DodgeGame;
-import com.mygdx.game.entities.Coin;
-import com.mygdx.game.entities.CoinCounter;
-import com.mygdx.game.entities.Timer;
-import com.mygdx.game.entities.Player;
+import com.mygdx.game.entities.*;
 
+import java.util.ArrayList;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Level {
@@ -20,13 +20,13 @@ public class Level {
     public static final int GRID_OFFSET_Y = 27;
     public static final int GRID_CORNER_SIZE = 84;//12*7
     public static final String[] DIRECTIONS = {"UP", "DOWN", "LEFT", "RIGHT"};
+    public static final Texture grid = new Texture("dodgeGrid.png");
     //public static final Music coinSound = Gdx.audio.newMusic(Gdx.files.internal("coin.mp3"));
     //public static final Music loseSound = Gdx.audio.newMusic(Gdx.files.internal("lose.mp3"));
 
     public String world, level;
     public Music music;
     public DodgeGame game;
-    public Texture grid;
     public Timer timer;
     public CoinCounter coinCounter;
     public Player player;
@@ -34,4 +34,182 @@ public class Level {
     public CopyOnWriteArrayList<Coin> coinList = new CopyOnWriteArrayList<Coin>();
     public int coinSpawnInterval;
     public boolean coinsSpawned;
+    public int minBoulders;
+    public int maxBoulders;
+    public int boulderSpawnInterval;
+    public int boulderSpawnDelay;
+    public boolean bouldersSpawned;
+    public CopyOnWriteArrayList<Boulder> boulderList = new CopyOnWriteArrayList<Boulder>();;
+    public CopyOnWriteArrayList<BoulderArrow> boulderArrowList = new CopyOnWriteArrayList<BoulderArrow>();
+
+    public void show(){
+        timer = new Timer(60);
+        coinCounter = new CoinCounter();
+    }
+
+    public void playMusic() {
+        music = Gdx.audio.newMusic(Gdx.files.internal("spinAndBurst.mp3"));
+        music.setLooping(true);
+        music.setVolume(1f);
+        music.play();
+    }
+
+    public void createPlayer() {
+        player = new Player();
+        player.x = 4;
+        player.y = 4;
+    }
+
+    public void displayTimer(float delta) {
+        timer.update(delta);
+        timer.render(game.batch, game.font);
+
+        //checks if time is up
+        if (timer.getWorldTimer() <= 0) {
+            music.stop();
+            music.dispose();
+            game.setScreen(new Start(game));
+        }
+    }
+
+    public void displayCoinCounter() {
+        coinCounter.update(coins);
+        coinCounter.render(game.batch, game.font);
+    }
+
+    public void displayWorldAndLevel() {
+        game.font.setColor(Color.PURPLE);
+        game.font.getData().setScale(4f);
+        game.font.draw(game.batch, world, (int)(DodgeGame.WIDTH * 0.07) , DodgeGame.HEIGHT/2 + 300);
+        game.font.draw(game.batch, level, (int)(DodgeGame.WIDTH * 0.25) , DodgeGame.HEIGHT/2 + 300);
+    }
+
+    public void drawGrid() {
+        game.batch.draw(grid, GRID_OFFSET_X, GRID_OFFSET_Y, GRID_WIDTH, GRID_HEIGHT);
+    }
+
+    public void detectCoin() {
+        for (int i = 0; i < coinList.size(); i++) {
+            if (coinList.get(i).x == player.x && coinList.get(i).y == player.y) {
+                coins++;
+                //coinSound.play();
+                coinList.remove(i);
+            }
+        }
+    }
+
+    public void renderPlayer() {
+        player.update();
+        player.render(game.batch);
+    }
+
+    public void renderCoins() {
+        for (Coin c : coinList) {
+            c.update();
+            c.render(game.batch);
+        }
+    }
+
+    public void renderBoulders(float delta) {
+        for (int i = 0; i < boulderList.size(); i++) {
+            //only updates and renders once the boulder is spawned
+            if (boulderList.get(i).spawned) {
+                //updates and render if the boulder is on screen
+                if (boulderList.get(i).isOnScreen) {
+                    boulderList.get(i).update(delta);
+                    boulderList.get(i).render(game.batch);
+                    //deletes boulder and arrowTexture once the boulder leaves the screen
+                } else {
+                    boulderList.remove(i);
+                    boulderArrowList.remove(i);
+                    i--;
+                }
+                //if the boulder hasn't spawned yet, the arrowTexture will blink
+                //(it will stop blinking after the boulder is spawned because it exits this else statement)
+            } else {
+                boulderArrowList.get(i).render(game.batch);
+                if (boulderArrowList.get(i).elapsedTime > boulderSpawnDelay)
+                    boulderList.get(i).spawned = true;
+            }
+        }
+    }
+
+
+    public void spawnCoins(){
+        if (timer.getWorldTimer() % coinSpawnInterval == 0 && !coinsSpawned){
+            coinList.clear();
+            for (int i = 0; i < 2; i++){
+                coinList.add(new Coin());
+                if (i == 1 && coinList.get(0).x == coinList.get(1).x && coinList.get(0).y == coinList.get(1).y){
+                    i--;
+                    coinList.remove(1);
+                }
+            }
+            coinsSpawned = true;
+        }
+        if (timer.getWorldTimer() % coinSpawnInterval != 0){
+            coinsSpawned = false;
+        }
+    }
+
+    public void spawnBoulders() {
+        if (timer.getWorldTimer() % boulderSpawnInterval == 0 && !bouldersSpawned){
+            //x and y lists to test if it's trying spawn a boulder where one already exists
+            ArrayList<Integer> xList = new ArrayList<Integer>();
+            ArrayList<Integer> yList = new ArrayList<Integer>();
+
+            for (int i = 0; i < (int) ((maxBoulders - minBoulders + 1) * Math.random() + minBoulders); i++) {
+                int x = 0;
+                int y = 0;
+                String direction = DIRECTIONS[(int) (Math.random() * 4)];
+
+                // randomly assigns a spawn position to the boulder based on what the direction of the boulder is
+                if (direction == "UP") {
+                    x = (int) (Math.random() * 8);
+                    y = -1;
+                } else if (direction == "DOWN") {
+                    x = (int) (Math.random() * 8);
+                    y = 8;
+                } else if (direction == "LEFT") {
+                    x = 8;
+                    y = (int) (Math.random() * 8);
+                } else if (direction == "RIGHT") {
+                    x = -1;
+                    y = (int) (Math.random() * 8);
+                }
+
+                //does not create a boulder if one is already there
+                boolean inList = false;
+                for (int tempX : xList) {
+                    for (int tempY : yList) {
+                        if (x == tempX && y == tempY) {
+                            inList = true;
+                        }
+                    }
+                }
+                if (!inList) {
+                    boulderList.add(new Boulder(x, y, direction));
+                    boulderArrowList.add(new BoulderArrow(x, y, direction));
+                    xList.add(x);
+                    yList.add(y);
+                } else {
+                    i--;
+                }
+            }
+            bouldersSpawned = true;
+        }
+        if (timer.getWorldTimer() % boulderSpawnInterval != 0){
+            bouldersSpawned = false;
+        }
+    }
+
+    public void detectBoulderCollision() {
+        for (int i = 0; i < boulderList.size(); i++) {
+            if (boulderList.get(i).x == player.x && boulderList.get(i).y == player.y) {
+                //loseSound.play()
+                music.dispose();
+                game.setScreen(new Start(game));
+            }
+        }
+    }
 }
--- core/src/com/mygdx/game/com/mygdx/game/screens/Level1.java
+++ core/src/com/mygdx/game/com/mygdx/game/screens/Level1.java
@@ -12,208 +12,61 @@ import java.util.ArrayList;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Level1 extends Level implements Screen{
-    private CopyOnWriteArrayList<Boulder> boulderList = new CopyOnWriteArrayList<Boulder>();;
-    private CopyOnWriteArrayList<BoulderArrow> boulderArrowList = new CopyOnWriteArrayList<BoulderArrow>();
-
-    public static final int MIN_BOULDERS = 3;
-    public static final int MAX_BOULDERS = 6;
-    public static final int BOULDER_SPAWN_INTERVAL = 5;
-    public static final int BOULDER_SPAWN_DELAY = 2;
-    private boolean bouldersSpawned;
 
     public Level1(DodgeGame g) {
         game = g;
-        grid = new Texture("dodgeGrid.png");
         world = "GRASS";
         level = " LEVEL 1";
         coinSpawnInterval = 10;
+        minBoulders = 3;
+        maxBoulders = 6;
+        boulderSpawnInterval = 5;
+        boulderSpawnDelay = 2;
 
-        music = Gdx.audio.newMusic(Gdx.files.internal("spinAndBurst.mp3"));
-        music.setLooping(true);
-        music.setVolume(1f);
-        music.play();
-
-        player = new Player();
-        player.x = 4;
-        player.y = 4;
-    }
-
-    public void show(){
-        timer = new Timer(60);
-        coinCounter = new CoinCounter();
+        playMusic();
+        createPlayer();
     }
 
     public void render(float delta) {
+        //rithesh i know youre doing something with the background here so imma leave this here
+
         //creates background
         Gdx.gl.glClearColor(.135f, .206f, .235f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        //display timer
-        timer.update(delta);
-        timer.render(game.batch, game.font);
 
-        //checks if time is up
-        if (timer.getWorldTimer() <= 0) {
-            music.stop();
-            this.dispose();
-            game.setScreen(new Start(game));
-        }
 
-        //display coin counter
-        coinCounter.update(coins);
-        coinCounter.render(game.batch, game.font);
 
-        //draws the grid
-        game.batch.draw(grid, GRID_OFFSET_X, GRID_OFFSET_Y, GRID_WIDTH, GRID_HEIGHT);
-
-        spawnEntities();
-        
-        renderEntities(delta);
+        //don't mess around with the order of the display methods unless you know what you're doing
+        //it will cause a SpriteBatch.begin must be called before draw or SpriteBatch music be called before end error
+        //for some reason displayTimer music be called first
+        displayTimer(delta);
+        displayWorldAndLevel();
+        drawGrid();
+        displayCoinCounter();
 
         detectCollision();
-
         detectCoin();
 
-        game.font.setColor(Color.PURPLE);
-        game.font.getData().setScale(4f);
-        game.font.draw(game.batch, world, (int)(DodgeGame.WIDTH * 0.07) , DodgeGame.HEIGHT/2 + 300);
-        game.font.draw(game.batch, level, (int)(DodgeGame.WIDTH * 0.25) , DodgeGame.HEIGHT/2 + 300);
+        spawnEntities();
+        renderEntities(delta);
+
         game.batch.end();
     }
 
-    public void spawnCoins(){
-        coinList.clear();
-        for (int i = 0; i < 2; i++){
-            coinList.add(new Coin());
-            if (i == 1 && coinList.get(0).x == coinList.get(1).x && coinList.get(0).y == coinList.get(1).y){
-                i--;
-                coinList.remove(1);
-            }
-        }
-    }
-
-    //spawns boulders and blinking arrows
-    public void spawnBoulders(){
-        //x and y lists to test if it's trying spawn a boulder where one already exists
-        ArrayList<Integer> xList = new ArrayList<Integer>();
-        ArrayList<Integer> yList = new ArrayList<Integer>();
-
-        for (int i = 0; i < (int)((MAX_BOULDERS - MIN_BOULDERS + 1) * Math.random() + MIN_BOULDERS); i++){
-            int x = 0;
-            int y = 0;
-            String direction = DIRECTIONS[(int)(Math.random() * 4)];
-
-            // randomly assigns a spawn position to the boulder based on what the direction of the boulder is
-            if (direction == "UP"){
-                x = (int)(Math.random() * 8);
-                y = -1;
-            }
-            else if (direction == "DOWN"){
-                x = (int)(Math.random() * 8);
-                y = 8;
-            }
-            else if (direction == "LEFT"){
-                x = 8;
-                y = (int)(Math.random() * 8);
-            }
-            else if (direction == "RIGHT"){
-                x = -1;
-                y = (int)(Math.random() * 8);
-            }
-
-            //does not create a boulder if one is already there
-            boolean inList = false;
-            for (int tempX : xList) {
-                for (int tempY : yList) {
-                    if (x == tempX && y == tempY) {
-                        inList = true;
-                    }
-                }
-            }
-            if (!inList) {
-                boulderList.add(new Boulder(x, y, direction));
-                boulderArrowList.add(new BoulderArrow(x, y, direction));
-                xList.add(x);
-                yList.add(y);
-            } else {
-                i--;
-            }
-        }
-    }
-
     public void spawnEntities() {
-        //spawns coins
-        if (timer.getWorldTimer() % coinSpawnInterval == 0 && !coinsSpawned){
-            spawnCoins();
-            coinsSpawned = true;
-        }
-        if (timer.getWorldTimer() % coinSpawnInterval != 0){
-            coinsSpawned = false;
-        }
-
-        //spawns boulders
-        if (timer.getWorldTimer() % BOULDER_SPAWN_INTERVAL == 0 && !bouldersSpawned){
-            spawnBoulders();
-            bouldersSpawned = true;
-        }
-        if (timer.getWorldTimer() % BOULDER_SPAWN_INTERVAL != 0){
-            bouldersSpawned = false;
-        }
+        spawnCoins();
+        spawnBoulders();
     }
 
     public void renderEntities(float delta) {
-        //update and renders the player
-        player.update();
-        player.render(game.batch);
-
-        //updates and renders each coin in the list
-        for (Coin c : coinList) {
-            c.update();
-            c.render(game.batch);
-        }
-
-        //updates and renders each boulder and arrowTexture in the list
-        for (int i = 0; i < boulderList.size(); i++) {
-            //only updates and renders once the boulder is spawned
-            if (boulderList.get(i).spawned) {
-                //updates and render if the boulder is on screen
-                if (boulderList.get(i).isOnScreen) {
-                    boulderList.get(i).update(delta);
-                    boulderList.get(i).render(game.batch);
-                //deletes boulder and arrowTexture once the boulder leaves the screen
-                } else {
-                    boulderList.remove(i);
-                    boulderArrowList.remove(i);
-                    i--;
-                }
-            //if the boulder hasn't spawned yet, the arrowTexture will blink
-            //(it will stop blinking after the boulder is spawned because it exits this else statement)
-            } else {
-                boulderArrowList.get(i).render(game.batch);
-                if (boulderArrowList.get(i).elapsedTime > BOULDER_SPAWN_DELAY)
-                    boulderList.get(i).spawned = true;
-            }
-        }
+        renderPlayer();
+        renderCoins();
+        renderBoulders(delta);
     }
 
     public void detectCollision() {
-        for (int i = 0; i < boulderList.size(); i++) {
-            if (boulderList.get(i).x == player.x && boulderList.get(i).y == player.y) {
-                //loseSound.play()
-                this.dispose();
-                game.setScreen(new Start(game));
-            }
-        }
-    }
-
-    public void detectCoin() {
-        for (int i = 0; i < coinList.size(); i++) {
-            if (coinList.get(i).x == player.x && coinList.get(i).y == player.y) {
-                coins++;
-                //coinSound.play();
-                coinList.remove(i);
-            }
-        }
+        detectBoulderCollision();
     }
 
     public void resize(int width, int height){
@@ -229,7 +82,6 @@ public class Level1 extends Level implements Screen{
 
     }
     public void dispose(){
-        music.dispose();
-        grid.dispose();
+
     }
 }
